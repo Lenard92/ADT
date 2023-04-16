f870ae4869cb66b0465942c064ccf6a793734bba
Lenar <lvanwinssen@icreative.nl>
2023-04-16 18:23:57 +0300

    Added images for Raspberry and Implemented  DT

--- React-SPA/my-app/src/App.js
+++ React-SPA/my-app/src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import logo1 from './RaspberryTwin_off.png';
+import logo2 from './RaspberryTwin_on.png';
 import './App.css';
 
 function App() {
+  const [image, setImage] = useState(logo1);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setImage((prevImage) => prevImage === logo1 ? logo2 : logo1);
+    }, 500);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Raspberry Pi Digital Twin</h1>
+        <img src={image} className="App-logo" alt="logo" />
+        <p>Temperature:</p>
+        <p>Humidity:</p>
       </header>
     </div>
   );
